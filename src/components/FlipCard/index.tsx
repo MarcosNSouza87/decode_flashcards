7a68a65835200e17b3ad9c1d0065337850b38ccd
Marcos.Souza <m_souza@outlook.com.br>
2022-11-02 14:12:24 -0300

    feature/add_lib_reanimated

--- src/components/FlipCard/index.tsx
+++ src/components/FlipCard/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import {Pressable} from 'react-native';
+import {
+  useSharedValue,
+  useAnimatedStyle,
+  interpolate,
+  withTiming,
+} from 'react-native-reanimated';
+import {styles} from './styles';
 
 import {Card} from '../Card';
-import {styles} from './styles';
 
 type Props = {
   data: {
@@ -13,13 +19,35 @@ type Props = {
 };
 
 export function FlipCard({data}: Props) {
-  const [isFlipped, setIsFlipped] = React.useState(false);
-  const handlePress = () => {
-    setIsFlipped(!isFlipped);
+  const flipPosition = useSharedValue(0);
+
+  const frontCardAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          rotateY: `${interpolate(flipPosition.value, [0, 1], [0, 180])}deg`,
+        },
+      ],
+    };
+  });
+  const backCardAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          rotateY: `${interpolate(flipPosition.value, [0, 1], [180, 360])}deg`,
+        },
+      ],
+    };
+  });
+
+  const handleFlipCard = () => {
+    const newValue = flipPosition.value === 0 ? 1 : 0;
+    flipPosition.value = withTiming(newValue, {duration: 500});
   };
   return (
-    <Pressable onPress={handlePress}>
-      {isFlipped ? <Card label={data.front} /> : <Card label={data.back} />}
+    <Pressable onPress={handleFlipCard}>
+      <Card label={data.front} style={frontCardAnimatedStyle} />
+      <Card label={data.back} style={[styles.back, backCardAnimatedStyle]} />
     </Pressable>
   );
 }
